eaf8f941250abaae2641456e2466ab6bb94280a2
tomov91 <tomov@webbuild.bg>
2018-04-16 16:19:11 +0300

    Added filter to the controller actions. Can be overwriten on need.

--- controllers/SlideController.php
+++ controllers/SlideController.php
@@ -10,6 +10,8 @@ use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use yii\web\UploadedFile;
+use yii\filters\AccessControl;
+use yii\filters\AccessRule;
 
 /**
  * SlideController implements the CRUD actions for Slide model.
@@ -22,6 +24,15 @@ class SlideController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
